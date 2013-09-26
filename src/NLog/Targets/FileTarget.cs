efef07ead4a6648752f59636519f5cd8a2b345df
Xharze <kimworking@gmail.com>
2013-09-27 01:11:53 +0200

    Remove extra compiler directive

--- src/NLog/Targets/FileTarget.cs
+++ src/NLog/Targets/FileTarget.cs
@@ -291,7 +291,6 @@ namespace NLog.Targets
         /// <docgen category='Output Options' order='10' />
         [Advanced]
         public Win32FileAttributes FileAttributes { get; set; }
-#endif
 
         /// <summary>
         /// Gets or sets the line ending mode.
