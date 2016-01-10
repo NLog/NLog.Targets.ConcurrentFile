09074e40744603e82a327555a50f9bfc8eede3c3
Werner van Deventer <werner@brutaldev.com>
2016-01-10 21:36:38 +0200

    Updates based on bhaeussermann's code review.

--- src/NLog/Targets/FileTarget.cs
+++ src/NLog/Targets/FileTarget.cs
@@ -1358,8 +1358,7 @@ namespace NLog.Targets
                 {
                     string archiveFileName = Path.GetFileName(nextFile);
                     int lastIndexOfStar = fileNameMask.LastIndexOf('*');
-
-                    // Safety check for issue #1147.
+          
                     if (lastIndexOfStar + dateFormat.Length <= archiveFileName.Length)
                     {
                         string datePart = archiveFileName.Substring(lastIndexOfStar, dateFormat.Length);
