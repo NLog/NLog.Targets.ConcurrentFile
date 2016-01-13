7617ab7958746c96939bcb3b5d4ec3952376aa08
bernhardhaeussermann <bernhardgh@icloud.com>
2016-01-13 20:34:47 +0200

    Fix and test archiving when writing to same file from different processes. Review feedback.

--- src/NLog/Targets/FileTarget.cs
+++ src/NLog/Targets/FileTarget.cs
@@ -579,6 +579,11 @@ namespace NLog.Targets
             }
         }
 
+        /// <summary>
+        /// Refresh the `WatchExternalFileArchiving` option of the <see cref="FileAppenderCache" />. 
+        /// The log file must be watched for archiving when multiple processes are writing to the same 
+        /// open file.
+        /// </summary>
         private void RefreshWatchExternalFileArchiving()
         {
 #if !SILVERLIGHT && !__IOS__ && !__ANDROID__
